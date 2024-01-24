6f759092fd4bef158d4df3c1851587ed18b6b073
Gradess <trofimovjava@gmail.com>
2024-01-25 00:15:48 +0100

    [BEST-15] Use different capture method

--- Source/BlueprintScreenshotTool/Public/BlueprintScreenshotToolHandler.h
+++ Source/BlueprintScreenshotTool/Public/BlueprintScreenshotToolHandler.h
@@ -27,12 +27,12 @@ public:
 	static void OpenDirectory();
 
 protected:
-	static TSharedRef<SWindow> CreateTransparentWindow(const FVector2D& InWindowSize);
-	static TSharedRef<SWindow> CreateTransparentWindowWithContent(const FVector2D& InWindowSize, TSharedRef<SWidget> InContent);
-	static void ShowWindow(TSharedRef<SWindow> InWindow);
-	static void FixGraphNodesAppearance(TSharedPtr<SGraphEditor> InGraphEditor);
+	static void RestoreNodeSelection(TSharedPtr<SGraphEditor> InGraphEditor, const TSet<UObject*>& InSelectedNodes);
 	static bool HasAnySelectedNodes(const TSet<TSharedPtr<SGraphEditor>>& InGraphEditors);
 	static void ShowNotification(const TArray<FString>& InPaths);
 	static void ShowDirectoryErrorNotification(const FString& InPath);
+	static UTextureRenderTarget2D* DrawGraphEditor(TSharedPtr<SGraphEditor> InGraphEditor, const FVector2D& InWindowSize);
 	static FString GetExtension(EBSTImageFormat InFormat);
 };
+
+
